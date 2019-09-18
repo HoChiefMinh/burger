49b99cf967cb0cdd86972722641c77c97df59928
Minh Pham <mqpham848@gmail.com>
2019-09-18 17:30:37 -0400

    set up structure

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+/* Seeds for SQL table. */
+USE burgers_db
+
+-- Inserted a set of records into the table
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Good Burger-Burger");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Crabby Patty");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Kahuna Burger");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("In-N-Out Burger");
