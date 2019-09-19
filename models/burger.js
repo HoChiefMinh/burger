d1a4ca3bd158ba92fc3686e601b5941e1a54ced5
Minh Pham <mqpham848@gmail.com>
2019-09-19 12:50:10 -0400

    added burgers.js file

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+const connection = require("../config/orm");
+
+let burger = {
+selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+        cb(res);
+    });
+}
+
+insertOne: function(cols, vals, cb) {
+    orm.selectAll("burgers", cols, vals, function(res) {
+        cb(res);
+    });
+}
+updateOne: function(objColVals, condition, cb) {
+    orm.selectAll("burgers", objColsVals, condition, function(res){
+        cb(res);
+    });
+}
+deleteOne: function(condition, cb) {
+    orm.selectAll("burgers", condition, function(res) {
+        cb(res);
+    })
+}
+};
+module.exports = burgers;
